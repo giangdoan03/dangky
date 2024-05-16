c7183b5c88aee8cdfbb68f9bfbb7731c118e6898
Giang Đoàn <doangiang665@gmail.com>
2024-05-16 17:31:55 +0700

    update 3

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -1,7 +1,7 @@
 <?php
 $servername='localhost';
 $username='root';
-$password='';
+$password='23432434';
 $dbname = "oksqpeoc_tonggiang";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 mysqli_set_charset($conn, 'UTF8');
