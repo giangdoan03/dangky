550b1f8bb138ff2640d5b588de61af5d3ee7cb61
Giang Doan <dvg8.vcc@gmail.com>
2024-05-30 11:02:12 +0700

    update

--- admin/send_emails.php
+++ admin/send_emails.php
@@ -117,7 +117,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
         <p style='margin: 5px 0'>Lưu ý: Thẻ dự kiểm tra thí sinh sẽ nhận tại phòng kiểm tra</p>
         <p>Trân trọng!</p>
         ";
-        $subject = "Thông báo SBD- Thời Gian - Địa điểm dự kiểm tra";
+        $subject = "Thông báo SBD - Thời Gian - Địa điểm dự kiểm tra";
 
         foreach ($batches as $batch) {
             sendBulkEmail($batch, $template, $subject);
