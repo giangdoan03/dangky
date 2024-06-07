0637daf6f95a4f0835d9e0583b9ff14c607bf0e4
Giang Doan <dvg8.vcc@gmail.com>
2024-06-07 15:00:52 +0700

    update

--- index.php
+++ index.php
@@ -893,7 +893,7 @@ function base_url()
         </div>
     </div>
     <footer id="footer" class="footer-wrapper">
-        <div class="is-divider divider clearfix has-block" style="max-width: 100%;background-color: rgb(0, 0, 0);display: block;height: 3px;margin: 1em 0;"></div>
+        <div class="is-divider divider clearfix has-block" style="max-width: 100%;background-color: #028900;display: block;height: 3px;margin: 1em 0;"></div>
         <div class="container">
             <section class="section" id="section_855073435">
                 <div class="bg section-bg fill bg-fill bg-loaded bg-loaded">
@@ -901,9 +901,9 @@ function base_url()
                 <div class="section-content relative">
                     <div class="row" id="row-1982555116">
                         <div id="col-525487658" class="col medium-6 small-12 large-6">
-                            <div class="col-inner">
+                            <div class="col-inner" style="line-height: 35px">
                                 <h2><strong>Trường THCS Thanh Xuân</strong></h2>
-                                <p>Địa chỉ: 203 Nguyễn Huy Tưởng, Thanh Xuân Trung, Thanh Xuân, Hà Nội</p>
+                                <p style="margin-bottom: 0">Địa chỉ: 203 Nguyễn Huy Tưởng, Thanh Xuân Trung, Thanh Xuân, Hà Nội</p>
                                 <div id="stack-3135118932" class="stack stack-col justify-start items-start">
                                     <a href="tel:0921.668.555" target="_self" class="button primary is-link lowercase">
                                         <i class="icon-phone" aria-hidden="true"></i> <span>Hotline: 0921.668.555</span>
@@ -917,7 +917,7 @@ function base_url()
                             </div>
                         </div>
                         <div id="col-249835136" class="col medium-3 small-12 large-3">
-                            <div class="col-inner">
+                            <div class="col-inner" style="line-height: 35px">
                                 <h3><strong>Địa chỉ</strong></h3>
                                 <div class="ux-menu stack stack-col justify-start">
                                     <div class="ux-menu-link flex menu-item ux-menu-link--active">
@@ -935,8 +935,12 @@ function base_url()
                         <div id="col-1203351300" class="col medium-3 small-12 large-3">
                             <div class="col-inner">
                                 <h3><strong>Kết nối với chúng tôi</strong></h3>
-                                <div class="social-icons follow-icons">
-                                    <a href="http://url" class="icon primary button circle facebook tooltip tooltipstered" ><i class="icon-facebook"></i></a>
+                                <div class="social-icons" style="margin-top: 20px">
+                                    <a href="https://www.facebook.com/thcsthanhxuan.edu.vn/" class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#ffffff" class="bi bi-facebook" viewBox="0 0 16 16">
+                                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                                        </svg>
+                                    </a>
                                     <a href="http://url" class="icon primary button circle instagram tooltip" ><i class="icon-instagram"></i></a>
                                     <a href="http://url" class="icon primary button circle twitter tooltip"><i class="icon-twitter"></i></a>
                                     <a href="mailto:your@email" class="icon primary button circle email tooltip"><i class="icon-envelop"></i></a></div>
