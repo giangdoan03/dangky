955499452b1301febe33584b695157f1fb640d89
Giang Doan <dvg8.vcc@gmail.com>
2024-06-14 15:41:27 +0700

    update

--- admin/send_batch_trung_tuyen.php
+++ admin/send_batch_trung_tuyen.php
@@ -74,64 +74,60 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
 
     $template = "
-        <table role=\"presentation\" style=\"width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;\">
+       <table role=\"presentation\" style=\"width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;\">
 		<tr>
 			<td align=\"center\" style=\"padding:0;\">
 				<table role=\"presentation\" style=\"width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;\">
 					<tr>
-						<td align=\"center\" style=\"padding:40px 0 30px 0;background:#70bbd9;\">
-							<img src=\"https://quantri.quangich.com/UploadImages/Config/thcsthanhxuan/Banner2.png\" alt=\"\" width=\"300\" style=\"height:auto;display:block;\" />
+						<td align=\"center\">
+							<img src=\"https://quantri.quangich.com/UploadImages/Config/thcsthanhxuan/Banner2.png\" alt=\"\" width=\"300\" style=\"height:auto;display:block;width: 100%;\" />
 						</td>
 					</tr>
 					<tr>
 						<td style=\"padding:36px 30px 42px 30px;\">
 							<table role=\"presentation\" style=\"width:100%;border-collapse:collapse;border:0;border-spacing:0;\">
 								<tr>
-									<td style=\"padding:0 0 36px 0;color:#153643;\">
-										<h1 style=\"font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;\">Creating Email Magic</h1>
-										<p style=\"margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan et dictum, nisi libero ultricies ipsum, posuere neque at erat.</p>
-										<p style=\"margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\"><a href=\"http://www.example.com\" style=\"color:#ee4c50;text-decoration:underline;\">In tempus felis blandit</a></p>
+									<td colspan=\"2\" style=\"padding:0;color:#153643;\">
+										<h1 style=\"font-size:18px;margin:0;font-family:Arial,sans-serif;text-align:center;margin-bottom: 20px;\">THÔNG BÁO TRÚNG TUYỂN VÀO LỚP 6 NĂM HỌC 2024-2025</h1>
+										<p style=\"margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\">Trường THCS CLC Thanh Xuân chúc mừng em: NGUYỄN VĂN A</p>
 									</td>
 								</tr>
-								<tr>
-									<td style=\"padding:0;\">
-										<table role=\"presentation\" style=\"width:100%;border-collapse:collapse;border:0;border-spacing:0;\">
-											<tr>
-												<td style=\"width:260px;padding:0;vertical-align:top;color:#153643;\">
-													<p style=\"margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\"><img src=\"https://assets.codepen.io/210284/left.gif\" alt=\"\" width=\"260\" style=\"height:auto;display:block;\" /></p>
-													<p style=\"margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, est nisi libero ultricies ipsum, in posuere mauris neque at erat.</p>
-													<p style=\"margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\"><a href=\"http://www.example.com\" style=\"color:#ee4c50;text-decoration:underline;\">Blandit ipsum volutpat sed</a></p>
-												</td>
-												<td style=\"width:20px;padding:0;font-size:0;line-height:0;\">&nbsp;</td>
-												<td style=\"width:260px;padding:0;vertical-align:top;color:#153643;\">
-													<p style=\"margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\"><img src=\"https://assets.codepen.io/210284/right.gif\" alt=\"\" width=\"260\" style=\"height:auto;display:block;\" /></p>
-													<p style=\"margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\">Morbi porttitor, eget est accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed.</p>
-													<p style=\"margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;\"><a href=\"http://www.example.com\" style=\"color:#ee4c50;text-decoration:underline;\">In tempus felis blandit</a></p>
-												</td>
-											</tr>
-										</table>
-									</td>
-								</tr>
+                                <tr>
+                                    <td><p style=\"margin: 0;\">Ngày sinh: 01/01/2013</p></td>
+                                    <td><p style=\"margin: 0;\">Số báo danh: TX0000</p></td>
+                                </tr>
+                                <tr>
+                                    <td colspan=\"2\"><p style=\"margin: 0;\"><strong>Đã trúng tuyển vào lớp 6 năm học 2024-2025 với tổng điểm: 60</strong></p></td>
+                                </tr>
+                                <tr>
+                                    <td colspan=\"2\">
+                                        <p style=\"margin: 0; line-height: 25px;\">Thời gian nộp hồ sơ nhập học:  <strong>ngày 19, 20/06/2024 (Sáng từ 7h30 đến 11h00, Chiều từ 14h00 đến 17h00)</strong></p>
+                                        <p style=\"margin: 0; line-height: 25px;\">Hồ sơ nhập học bao gồm:</p>
+                                        <p style=\"margin: 0; line-height: 25px;\"><strong>+ Bản chính học bạ cấp Tiểu học.</strong></p>
+<p style=\"margin: 0; line-height: 25px;\"><strong>+ Phiếu kê khai thông tin học sinh (theo mẫu của nhà trường) kèm theo minh chứng về nơi thường trú (Căn cước công dân, truy cập định danh điện tử VNeID mức 2 của Bố (mẹ) hoặc người giám hộ học sinh đăng ký tuyển sinh hoặc giấy xác nhận thường trú do cơ quan có thẩm quyền cấp để đối chiếu)</strong></p>
+<p style=\"margin: 0; line-height: 25px;\"><strong>+ Bản sao giấy khai sinh</strong></p>
+<p style=\"margin: 0; line-height: 25px;\"><strong>+ Bản chính giấy xác nhận ưu tiên do cơ quan có thẩm quyền cấp (nếu có)</strong></p>
+<p style=\"margin: 0; line-height: 25px;\"><strong>(Trong trường hợp hồ sơ nhập học không khớp với Hồ sơ dự tuyển và không đúng với các quy định trong kế hoạch tuyển sinh của nhà trường kết quả trúng tuyển sẽ bị hủy)</strong></p>
+
+                                    </td>
+                                </tr>
 							</table>
 						</td>
 					</tr>
 					<tr>
-						<td style=\"padding:30px;background:#ee4c50;\">
+						<td style=\"padding:30px;background:#028900;\">
 							<table role=\"presentation\" style=\"width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;\">
 								<tr>
-									<td style=\"padding:0;width:50%;\" align=\"left\">
+									<td style=\"padding:0;width:100%;\" align=\"left\">
 										<p style=\"margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;\">
-											&reg; Someone, Somewhere 2024<br/><a href=\"http://www.example.com\" style=\"color:#ffffff;text-decoration:underline;\">Unsubscribe</a>
+											Copyright 2024 © Bản quyền thuộc về Trường THCS Thanh Xuân<br/><a href=\"http://www.example.com\" style=\"color:#ffffff;text-decoration:underline;\">Unsubscribe</a>
 										</p>
 									</td>
 									<td style=\"padding:0;width:50%;\" align=\"right\">
 										<table role=\"presentation\" style=\"border-collapse:collapse;border:0;border-spacing:0;\">
 											<tr>
 												<td style=\"padding:0 0 0 10px;width:38px;\">
-													<a href=\"http://www.twitter.com/\" style=\"color:#ffffff;\"><img src=\"https://assets.codepen.io/210284/tw_1.png\" alt=\"Twitter\" width=\"38\" style=\"height:auto;display:block;border:0;\" /></a>
-												</td>
-												<td style=\"padding:0 0 0 10px;width:38px;\">
-													<a href=\"http://www.facebook.com/\" style=\"color:#ffffff;\"><img src=\"https://assets.codepen.io/210284/fb_1.png\" alt=\"Facebook\" width=\"38\" style=\"height:auto;display:block;border:0;\" /></a>
+													<a href=\"https://www.facebook.com/thcsthanhxuan.edu.vn/\" style=\"color:#ffffff;\"><img src=\"https://assets.codepen.io/210284/fb_1.png\" alt=\"Facebook\" width=\"38\" style=\"height:auto;display:block;border:0;\" /></a>
 												</td>
 											</tr>
 										</table>
